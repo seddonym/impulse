a503b88c268c0ff08f84a1b9628f919a3e33fcf2
David Seddon <david.seddon@octoenergy.com>
2019-02-09 14:40:16 +0000

    Update setup.py development status to beta

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: Unix',
