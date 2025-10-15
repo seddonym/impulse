68dfdf7ead0856b544c35055a29ddecf2e0c5cd7
David Seddon <david.seddon@kraken.tech>
2025-10-15 15:01:13 +0100

    Drop support for Python 3.7 and 3.8

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,6 +1,11 @@
 Changelog
 =========
 
+latest
+------
+
+* Drop support for Python 3.7 and 3.8.
+
 1.0 (2022-10-4)
 ---------------
 
--- setup.py
+++ setup.py
@@ -46,8 +46,6 @@ setup(
         'Operating System :: POSIX',
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: Implementation :: CPython',
